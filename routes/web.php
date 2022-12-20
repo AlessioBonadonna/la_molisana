8ab692b34c8c7cd0413295b775b58099caa1af0c
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-12-20 11:06:28 +0100

    print title

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\HomeController as HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+Route::get('/products', [HomeController::class, 'index'])->name('products.index');
